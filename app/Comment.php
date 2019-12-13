1b16a80d032fffa8cca4678947eeefe1f3e8f18e
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-12-14 01:45:16 +0500

    comment model relations & sluggable implemented

--- app/Comment.php
+++ app/Comment.php
@@ -9,26 +9,26 @@ class Comment extends Model
 {
 	use Sluggable;
 
-		/**
-	 	* Return the sluggable configuration array for this model.
-     *
-     * @return array
-     */
-    public function sluggable()
-    {
-        return [
-            'slug' => [
-                'source' => 'title'
-            ]
-        ];
-    }
-    public function post()
-		{
-			return $this->hasOne(Post::class);
-		}
+	/**
+	 * Return the sluggable configuration array for this model.
+	 *
+	 * @return array
+	 */
+	public function sluggable()
+	{
+		return [
+			'slug' => [
+				'source' => 'title'
+			]
+		];
+	}
+	public function post()
+	{
+		return $this->hasOne(Post::class);
+	}
 
-		public function author()
-		{
-			return $this->hasOne(User::class);
-		}
+	public function author()
+	{
+		return $this->hasOne(User::class);
+	}
 }
