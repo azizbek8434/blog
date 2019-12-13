c22b7845bd42203812dd6e5b2c32fbbbd01e9ae2
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-12-14 01:36:24 +0500

    tag model relations implemented

--- app/Tag.php
+++ app/Tag.php
@@ -8,27 +8,27 @@ use Cviebrock\EloquentSluggable\Sluggable;
 class Tag extends Model
 {
 	use Sluggable;
-		/**
-     * Return the sluggable configuration array for this model.
-     *
-     * @return array
-     */
-    public function sluggable()
-    {
-        return [
-            'slug' => [
-                'source' => 'title'
-            ]
-        ];
-		}
-		
-    public function post()
-		{
-			return $this->belongsToMany(
-				Post::class,
-				'post_tags',
-				'tag_id',
-				'post_id'
-			);
-		}
+	/**
+	 * Return the sluggable configuration array for this model.
+	 *
+	 * @return array
+	 */
+	public function sluggable()
+	{
+		return [
+			'slug' => [
+				'source' => 'title'
+			]
+		];
+	}
+
+	public function post()
+	{
+		return $this->belongsToMany(
+			Post::class,
+			'post_tags',
+			'tag_id',
+			'post_id'
+		);
+	}
 }
