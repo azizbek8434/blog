1adc3f4cd5cf9ae0d9bf08db53690a0fbd07b650
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-12-14 01:33:48 +0500

    post model relations implented

--- app/Post.php
+++ app/Post.php
@@ -12,69 +12,71 @@ class Post extends Model
 
 	protected $fillable = ['title', 'content', 'user_id'];
 
-		/**
-     * Return the sluggable configuration array for this model.
-     *
-     * @return array
-     */
-    public function sluggable()
-    {
-        return [
-            'slug' => [
-                'source' => 'title'
-            ]
-        ];
-    }
+	/**
+	 * Return the sluggable configuration array for this model.
+	 *
+	 * @return array
+	 */
+	public function sluggable()
+	{
+		return [
+			'slug' => [
+				'source' => 'title'
+			]
+		];
+	}
 
-		public function add($fields)
-		{
-			$post = new static;
-			$post->fill($fields);
-			$post->user_id = 1;
-			$post->save();
-			
-			return $post;
-		}
+	public function add($fields)
+	{
+		$post = new static;
+		$post->fill($fields);
+		$post->user_id = 1;
+		$post->save();
 
-		public function edit($fields)
-		{
-			$this->fill($fields);
-			$this->save();
-		}
+		return $post;
+	}
 
-		public function remove()
-		{
-			Storage::delete('uploads/',$this->image);
-			$this->delete();
-		}
+	public function edit($fields)
+	{
+		$this->fill($fields);
+		$this->save();
+	}
 
-		public function uploadImage($image)
-		{
-			if($image == null){ return; }
-			Storage::delete('uploads/',$this->image);
-			$filename = str_rundom(10). '.'. $image->extension();
-			$image->saveAs('uploads', $filename);
-			$this->image = $filename;
-			$this->save();
-		}
-		
-		public function category()
-		{
-			return $this->hasOne(Category::class);
-		}
+	public function remove()
+	{
+		Storage::delete('uploads/', $this->image);
+		$this->delete();
+	}
 
-		public function author()
-		{
-			return $this->hasOne(User::class);
+	public function uploadImage($image)
+	{
+		if ($image == null) {
+			return;
 		}
+		Storage::delete('uploads/', $this->image);
+		$filename = str_rundom(10) . '.' . $image->extension();
+		$image->saveAs('uploads', $filename);
+		$this->image = $filename;
+		$this->save();
+	}
 
-		public function tags()
-		{
-			return $this->belongsToMany(
-				Tag::class,
-				'post_tags',
-				'post_id',
-				'tag_id'
-			);
-		}
+	public function category()
+	{
+		return $this->hasOne(Category::class);
+	}
+
+	public function author()
+	{
+		return $this->hasOne(User::class);
+	}
+
+	public function tags()
+	{
+		return $this->belongsToMany(
+			Tag::class,
+			'post_tags',
+			'post_id',
+			'tag_id'
+		);
+	}
 }
